25b300edf2855e9f99c3d74e007fe804b1871eb2
Alsingador <jberg99@live.se>
2026-04-14 21:46:42 +0200

    create public from static

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-from textnode import TextNode  
-from leafnode import LeafNode 
+import os
+import shutil
 
 
 def main():
-    text_node = TextNode("This is some anchor text", "link", url="https://www.boot.dev")
-    print(text_node)
-    
+    print("started")
+    set_up_directory("static", "public")
+    print("done")
+
+def set_up_directory(source, destination):
+    if not os.path.exists(source):
+        raise Exception(f"Missing source argument: {source}")
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
 
 
 if __name__ == "__main__":
